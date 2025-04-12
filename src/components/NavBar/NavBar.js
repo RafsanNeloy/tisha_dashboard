9488a422e433794815353185605b4e41839481f5
axif <muhamadasif570@gmail.com>
2025-04-12 21:56:46 +0600

    fix app drawer

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -24,12 +24,33 @@ import CollectionForm from '../CustomerPage/Collections/CollectionForm'
 import CustomerDetails from '../CustomerPage/customer_details'
 import MonthlyHistory from '../Dashboard/MonthlyHistory'
 import PrivateRoute from './PrivateRoute'
+import { Box } from '@mui/material'
+import { makeStyles } from '@mui/styles'
+import { useTheme } from '@mui/material/styles'
+import { useMediaQuery } from '@mui/material'
+
+const useStyles = makeStyles({
+  content: {
+    flexGrow: 1,
+    padding: '20px',
+    marginLeft: props => props.isLoggedIn ? (props.isMobile ? '0px' : props.drawerWidth) : '0px',
+    transition: 'margin 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms',
+    width: props => props.isLoggedIn ? `calc(100% - ${props.isMobile ? '0px' : props.drawerWidth}px)` : '100%',
+  }
+});
 
 const NavBar = (props) => {
     const isLoggedIn = useSelector(state => state.login)
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const location = useLocation()
+    
+    // Get the drawer width from localStorage or use default
+    const drawerWidth = localStorage.getItem('drawerWidth') || 260
+    const theme = useTheme()
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+    
+    const classes = useStyles({ isLoggedIn, drawerWidth, isMobile })
 
     useEffect(() => {
         if (localStorage.getItem('token')) {
@@ -46,24 +67,26 @@ const NavBar = (props) => {
     return(
         <div>
             {isLoggedIn ? <Drawer /> : <AppBar />}
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login-or-register" element={<LoginRegisterPage />} />
-                <Route path="/profile" element={<PrivateRoute><UserPage /></PrivateRoute>} />
-                <Route path="/user" element={<PrivateRoute><UserPage /></PrivateRoute>} />
-                <Route path="/customers" element={<PrivateRoute><CustomerPage /></PrivateRoute>} />
-                <Route path="/products" element={<PrivateRoute><ProductPage /></PrivateRoute>} />
-                <Route path="/bills" element={<PrivateRoute><BillsPage /></PrivateRoute>} />
-                <Route path="/addBill" element={<PrivateRoute><AddBill /></PrivateRoute>} />
-                <Route path="/customer-details/:customerId" element={<PrivateRoute><CustomerDetails /></PrivateRoute>} />
-                <Route path="/customers/:id" element={<PrivateRoute><ViewCustomer /></PrivateRoute>} />
-                <Route path="/bills/:id" element={<PrivateRoute><BillView /></PrivateRoute>} />
-                <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-                <Route path="/westage" element={<PrivateRoute><WestageForm /></PrivateRoute>} />
-                <Route path="/less" element={<PrivateRoute><LessForm /></PrivateRoute>} />
-                <Route path="/collections" element={<PrivateRoute><CollectionForm /></PrivateRoute>} />
-                <Route path="/history" element={<PrivateRoute><MonthlyHistory /></PrivateRoute>} />
-            </Routes>
+            <Box className={classes.content}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login-or-register" element={<LoginRegisterPage />} />
+                    <Route path="/profile" element={<PrivateRoute><UserPage /></PrivateRoute>} />
+                    <Route path="/user" element={<PrivateRoute><UserPage /></PrivateRoute>} />
+                    <Route path="/customers" element={<PrivateRoute><CustomerPage /></PrivateRoute>} />
+                    <Route path="/products" element={<PrivateRoute><ProductPage /></PrivateRoute>} />
+                    <Route path="/bills" element={<PrivateRoute><BillsPage /></PrivateRoute>} />
+                    <Route path="/addBill" element={<PrivateRoute><AddBill /></PrivateRoute>} />
+                    <Route path="/customer-details/:customerId" element={<PrivateRoute><CustomerDetails /></PrivateRoute>} />
+                    <Route path="/customers/:id" element={<PrivateRoute><ViewCustomer /></PrivateRoute>} />
+                    <Route path="/bills/:id" element={<PrivateRoute><BillView /></PrivateRoute>} />
+                    <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+                    <Route path="/westage" element={<PrivateRoute><WestageForm /></PrivateRoute>} />
+                    <Route path="/less" element={<PrivateRoute><LessForm /></PrivateRoute>} />
+                    <Route path="/collections" element={<PrivateRoute><CollectionForm /></PrivateRoute>} />
+                    <Route path="/history" element={<PrivateRoute><MonthlyHistory /></PrivateRoute>} />
+                </Routes>
+            </Box>
         </div>
     )
 }
