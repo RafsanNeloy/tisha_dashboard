76e3243ddf673de0244a83d5e0383b177f5a7f7c
axif <muhamadasif570@gmail.com>
2025-04-16 22:44:24 +0600

    may delete later

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -12,6 +12,7 @@ store.subscribe(() => {
   console.log('State updated', store.getState());
 });
 
+// React Router v6 future flags configuration
 const router = {
   future: {
     v7_startTransition: true,
@@ -19,11 +20,14 @@ const router = {
   }
 };
 
-ReactDOM.render(
+// Create root and render
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <BrowserRouter future={router}>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
